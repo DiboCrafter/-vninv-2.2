eeb70a23bef6d987db27d7a751a52e7030fb2673
DiboCrafter <emsv06002@utb.vaxjo.se>
2024-11-14 13:01:06 +0100

    Add project files.

---
+++ Övninv 2.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Övning2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur långt hoppade du i meter?");
+            string hoppade = Console.ReadLine();
+            int hoppdade= int.Parse( hoppade );
+            int suma = 3 + hoppdade; 
+            Console.WriteLine("du hoppade" + " " + hoppade + " " + "meter" + " " + "men Elin hoppade" + " " + suma + " " + "meter");
+
+        }
+    }
+}
